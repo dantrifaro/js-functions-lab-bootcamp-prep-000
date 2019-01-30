790a220e70c684adcbaaf1d83135f6230768ed54
Daniel J Trifaro <djtrifaro@gmail.com>
2019-01-30 18:17:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function happyHolidayTo(holiday, name) {
 happyHolidayTo('Independence Day', 'you');
 
 function holidayCountdown(days, holiday) {
-  return (`It's ${days} days until ${holiday}!`);
+  return (`It\'s ${days} days until ${holiday}!`);
 }
 
 holidayCountdown(20, 'Mother\'s Day');
